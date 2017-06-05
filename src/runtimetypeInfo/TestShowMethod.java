d8caaa370fa8b880d34662a6f4fd9cc2a156a475
LiuYI <liuyi@agoldcomm.com>
2017-06-05 14:12:22 +0800

    Thinking in Java reflect

---
+++ src/runtimetypeInfo/TestShowMethod.java
@@ -0,0 +1,12 @@
+package runtimetypeInfo;
+
+import runtimetypeInfo.*;
+/**
+ * Created by root on 17-6-5.
+ */
+public class TestShowMethod {
+    public static void main(String[] args){
+        String[] a = new String[]{"runtimetypeInfo.ShowMethods"};
+        ShowMethods.main(a);
+    }
+}
