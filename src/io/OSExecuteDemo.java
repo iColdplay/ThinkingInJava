12bf095f79cfdf74e031c4a075a475053e009eae
iColdplay <1059584227@qq.com>
2017-07-07 19:36:22 +0800

    Thinking in Java a little change

---
+++ src/io/OSExecuteDemo.java
@@ -0,0 +1,10 @@
+package io;
+
+/**
+ * Created by root on 17-7-7.
+ */
+public class OSExecuteDemo {
+    public static void main(String[] args){
+        OSExecute.command("javap /home/file_file/20170707_files_echo/Echo.java");
+    }
+}
